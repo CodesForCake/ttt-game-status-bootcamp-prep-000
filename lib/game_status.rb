4b6e1ac8563c6df00b359df6f462c70e02a45812
Mimi C <mina123fire@yahoo.com>
2020-08-29 07:22:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,10 +25,10 @@ def won?(board)
     position_2 = board[index_2]
     position_3 = board[index_3]
     
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X")||(position_1 == "O" && position_2 == "O" && position_3 == "O")
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X")||(position_1 == "O" && position_2 == "O" && position_3 == "")
       return win
     else
-      return false
+      false
     end
   end
 end
